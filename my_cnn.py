f6cb93b48bd3c2861145928b43ada9ee89d65f99
wuzy <184701326@qq.com>
2017-06-12 01:28:33 +0800

    add my_cnn

---
+++ my_cnn.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable 
+import torch.nn.functional as F
+import torch.utils.data as Data
+from input_file import getData,getLabel
+import pdb
+
+INPUT_SIZE = 28*28
+CLASS_NUM = 10
+BATCH_SIZE = 100
+EPOCH = 10
+
+train_x = torch.from_numpy(getData("train")/255.)
+train_y = torch.from_numpy(getLabel("train"))
+test_x = torch.from_numpy(getData("test")/255.)
+test_y = torch.from_numpy(getLabel("test"))
+
+train_minist_dataset = Data.TensorDataset( data_tensor=train_x,target_tensor=train_y)
+test_minist_dataset = Data.TensorDataset(data_tensor=test_x,target_tensor=test_y)
+
+
+train_loader = Data.DataLoader(
+    dataset= train_minist_dataset,
+    batch_size = BATCH_SIZE,
+    shuffle = True,
+
+)
+test_loader= Data.DataLoader(
+    dataset= test_minist_dataset,
+    batch_size = BATCH_SIZE,
+    shuffle = True,
+)
+
+class CNN(nn.Module):     
+    def __init__(self):
+        super(CNN,self).__init__()
+        self.conv1 = nn.Sequential(
+        nn.Conv2d(1,16,5,1,2),
+        nn.ReLU(),
+        nn.MaxPool2d(2),
+        )
+
+        self.conv2 = nn.Sequential(
+        nn.Conv2d(16,32,5,1,2),
+        nn.ReLU(),
+        nn.MaxPool2d(2),
+        )
+
+        self.out = nn.Linear(32*7*7,10)
+
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1) 
+        out = self.out(x)
+        return out
+
+
+cnn = CNN()
+optimizer = torch.optim.Adam(cnn.parameters(),)
+loss_func = nn.CrossEntropyLoss()
+
+for epoch in range(EPOCH):
+    for setp,(x,y) in enumerate(train_loader):
+        x = torch.Tensor.float(x)
+        b_x = Variable(x.view(BATCH_SIZE,1,28,28))
+        b_y = Variable(y.view(100))
+       # print(b_x,b_y)
+        output = cnn(b_x)
+        loss = loss_func(output,b_y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (setp+1) % 100 == 0:
+            print ('Epoch: [%d/%d], Step: [%d/%d], Loss: %.4f' 
+                   % (epoch+1, EPOCH, setp+1, len(train_minist_dataset)//BATCH_SIZE, loss.data[0]))
+
+
+correct = 0
+total = 0
+for images, labels in test_loader:
+    images = torch.Tensor.float(images)
+    images = Variable(images.view(BATCH_SIZE,1,28,28))
+    outputs = cnn(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
+    
+print('Accuracy of the model on the 10000 test images: %.4f %%' % (100 * correct / total))
+
--- old.py
+++ old.py
@@ -3,23 +3,22 @@ import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
-from torch.autograd import Variable 
+from torch.autograd import Variable
 import torch.nn.functional as F
 import torch.utils.data as Data
 from input_file import getData,getLabel
 import pdb
 # Hyper Parameters 
 input_size = 784
-hidden1_size = 500
-hidden2_size = 300
+hidden_size = 26
 num_classes = 10
-num_epochs = 20
+num_epochs = 10
 batch_size = 100
-learning_rate = 0.001
+learning_rate = 0.01
 
-train_x = torch.from_numpy(getData("train")/255.)
+train_x = torch.from_numpy(getData("train"))
 train_y = torch.from_numpy(getLabel("train"))
-test_x = torch.from_numpy(getData("test")/255.)
+test_x = torch.from_numpy(getData("test"))
 test_y = torch.from_numpy(getLabel("test"))
 
 train_minist_dataset = Data.TensorDataset( data_tensor=train_x,target_tensor=train_y)
@@ -28,49 +27,57 @@ train_loader = Data.DataLoader(
     dataset= train_minist_dataset,
     batch_size = batch_size,
     shuffle = True,
-   # num_workers=2,
+#    num_workers=2,
 
 )
 test_loader= Data.DataLoader(
     dataset= test_minist_dataset,
     batch_size = batch_size,
     shuffle = True,
-    #num_workers=2,
+ #   num_workers=2,
 
 )
 
 class LogisticRegression(nn.Module):
-    def __init__(self, input_size,hidden1_size,hidden2_size, num_classes):
+    def __init__(self, input_size,hidden_size, num_classes):
         super(LogisticRegression, self).__init__()
-        self.input = nn.Linear(input_size, hidden1_size)
-        self.hidden1 = nn.Linear(hidden1_size,hidden2_size)
-        self.hidden2 = nn.Linear(hidden2_size,num_classes)
+        self.input = nn.Linear(input_size, hidden_size)
+        self.hidden = nn.Linear(hidden_size,num_classes)
     def forward(self, x):
-        out1 = F.relu(self.input(x))
-        out2 = F.relu(self.hidden1(out1))
-        out3 = self.hidden2(out2)
-      #     out = self.linear(x)
-        return out3
+        out1 = F.sigmoid(self.input(x))
+        out2 = self.hidden(out1)
+      # 	out = self.linear(x)
+        return out2
 
-model = LogisticRegression(input_size,hidden1_size, hidden2_size, num_classes)
+model = LogisticRegression(input_size,hidden_size, num_classes)
 
 # Loss and Optimizer
 # Softmax is internally computed.
 # Set parameters to be updated.
-criterion = nn.CrossEntropyLoss()  
-optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)  
+#criterion = nn.MSELoss()  
+
+criterion = nn.MSELoss()  
+optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)  
+saveloss = []
 
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         images = torch.Tensor.float(images)
         images = Variable(images.view(-1,28*28))
-        labels = Variable(labels.view(100))
-       # print("epoch=",epoch,"i=",i,"images=",images.size(),"labels=",labels.size())
-       
+
+        labels_onehot = torch.zeros(labels.size(0),num_classes)
+        labels_onehot.scatter_(1,labels,1)
+
+        labels_onehot = Variable(torch.Tensor.float(labels_onehot))
+
+     #   print("epoch=",epoch,"i=",i,"images=",images.size(),"labels=",labels.size())
         # Forward + Backward + Optimize
         optimizer.zero_grad()
         outputs = model(images)
+       # pdb.set_trace
+       # print(outputs.size(),type(labels))
         loss = criterion(outputs, labels)
+        saveloss.append(loss.data[0])
         loss.backward()
         optimizer.step()
         
@@ -90,7 +97,13 @@ for images, labels in test_loader:
     total += labels.size(0)
     correct += (predicted == labels).sum()
     
-print('Accuracy of the model on the 10000 test images: %.4f %%' % (100 * correct / total))
+print('Accuracy of the model on the 10000 test images: %f %%'  %(100 * correct / total))
 
-# Save the Model
-torch.save(model.state_dict(), 'model.pkl')
+
+from matplotlib import pyplot as plt
+xaxis = [x for x in range(600*num_epochs)]
+plt.plot(xaxis,saveloss)
+plt.show()
+
+ #Save the Model
+torch.save(model, 'model.pkl')
